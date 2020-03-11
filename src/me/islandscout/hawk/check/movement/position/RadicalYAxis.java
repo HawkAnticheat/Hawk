2fe5ca1eedfe846048fa09db44b3f654f297924f
Islandscout <islandscout8@gmail.com>
2020-03-10 22:15:21 -0700

    Rename RadicalYAxis

--- src/me/islandscout/hawk/check/movement/position/RadicalYAxis.java
+++ src/me/islandscout/hawk/check/movement/position/RadicalYAxis.java
@@ -3,17 +3,17 @@ package me.islandscout.hawk.check.movement.position;
 import me.islandscout.hawk.check.MovementCheck;
 import me.islandscout.hawk.event.MoveEvent;
 
-public class RadicalY extends MovementCheck {
+public class RadicalYAxis extends MovementCheck {
 
-    public RadicalY() {
-        super("radicaly", "%player% failed radical-Y, VL: %vl%");
+    public RadicalYAxis() {
+        super("radicalyaxis", "%player% did a radical Y-axis, VL: %vl%");
     }
 
     @Override
     protected void check(MoveEvent e) {
         //This is totally radical, dude! Where'd you find this, at the 1980s?
         //No seriously, this is just a joke from the MAC discord. Someone took a
-        //screenshot of a post on the Hypixel forum. Whatever tf "radical y"
+        //screenshot of a post on the Hypixel forum. Whatever tf "radical y axis"
         //is supposed to mean is beyond me. Hawk has it, though!
 
         //if(MathPlus.isRadical(e.getTo().getY())) {
