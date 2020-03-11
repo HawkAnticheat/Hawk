90a093e1a208f8211000bbcead1651b46abdb798
Islandscout <islandscout8@gmail.com>
2020-03-10 22:31:48 -0700

    Improve RadicalYAxis

--- src/me/islandscout/hawk/check/movement/position/RadicalYAxis.java
+++ src/me/islandscout/hawk/check/movement/position/RadicalYAxis.java
@@ -15,10 +15,32 @@ public class RadicalYAxis extends MovementCheck {
         //No seriously, this is just a joke from the MAC discord. Someone took a
         //screenshot of a post on the Hypixel forum. Whatever tf "radical y axis"
         //is supposed to mean is beyond me. Hawk has it, though!
+        if(isRadical(e.getTo().getY())) {
+          e.getPlayer().setBanned(true);
+          e.getPlayer().kickPlayer("The 1980s is too rad for you, punk ass.");
+        }
+    }
 
-        //if(MathPlus.isRadical(e.getTo().getY())) {
-        //  e.getPlayer().setBanned(true);
-        //  e.getPlayer().kickPlayer("The 1980s is too rad for you, punk ass.");
-        //}
+    //math proofs... maybe?
+    private boolean isRadical(double x) {
+        for(int exp = 1; exp > 0; exp++) {
+            for(int root = 2; root > 0; root++) {
+                if(isRational(Math.pow(x, (double)root/exp)))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isRational(double x) {
+        for(int denominator = 1; denominator > 0; denominator++) {
+            if(isInteger(x * denominator))
+                return true;
+        }
+        return false;
+    }
+
+    private boolean isInteger(double x) {
+        return Math.round(x) - x == 0;
     }
 }
